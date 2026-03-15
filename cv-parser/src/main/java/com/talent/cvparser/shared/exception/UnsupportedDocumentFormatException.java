1ae7a84fa3a904e978fdce882e405403090041a9
LeQuangGiaBao <baolqg1309@gmail.com>
2026-03-15 17:51:37 +0700

    feat: Add DOCX document parsing with configurable security limits and a new `UnsupportedDocumentFormatException`.

--- cv-parser/src/main/java/com/talent/cvparser/shared/exception/UnsupportedDocumentFormatException.java
+++ cv-parser/src/main/java/com/talent/cvparser/shared/exception/UnsupportedDocumentFormatException.java
@@ -1,7 +1,4 @@
 package com.talent.cvparser.shared.exception;
 
-public class UnsupportedDocumentFormatException extends ParsingException{
-    public UnsupportedDocumentFormatException(String message) {
-        super(message);
-    }
+public class UnsupportedDocumentFormatException {
 }
