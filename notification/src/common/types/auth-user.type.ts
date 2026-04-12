a6675c895c763356b94650ba3b5e050194f6f3af
Kietlatoi <toilakiet24@gmail.com>
2026-04-12 11:14:04 +0700

    [12/04/2026] [KietDM] fix Phase 1 [1.1 1.2]

--- notification/src/common/types/auth-user.type.ts
+++
@@ -1,5 +0,0 @@
-export interface AuthUser {
-  id: string;
-  email?: string;
-  role?: string;
-}
--- notification/src/rabbitmq/rabbitmq.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { RabbitmqService } from './rabbitmq.service';
-
-@Global()
-@Module({
-  providers: [RabbitmqService],
-  exports: [RabbitmqService],
-})
-export class RabbitmqModule {}
